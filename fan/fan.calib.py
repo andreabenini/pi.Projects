e49ca4027fcd727cd2ec5703c379fa831a930711
Ben <andreabenini@users.noreply.github.com>
2019-08-05 12:36:20 +0200

    Update fan.calib.py

--- fan/fan.calib.py
+++ fan/fan.calib.py
@@ -3,19 +3,17 @@
 #
 # Interactive utility for testing PWM and fan duty cycle
 #
-
 import sys
 import time
 import RPi.GPIO as GPIO
 
-FAN_PIN = 18
-WAIT_TIME = 1
-PWM_FREQ = 25
+FAN_PIN  = 18                   # GPIO 18, PWM, pin 12
+PWM_FREQ = 25                   # PWM frequency
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-fan=GPIO.PWM(FAN_PIN, PWM_FREQ)
+fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
 fan.start(0);
 try:
     print("Enter Duty Cycle (0..100)")
